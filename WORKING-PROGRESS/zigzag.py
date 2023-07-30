32709eeaa92c64380f143a84dd8f5cbf29e17764
BradleyGilden <bradleygilden@gmail.com>
2023-07-30 19:24:48 +0200

    work-in-progress

--- WORKING-PROGRESS/zigzag.py
+++ WORKING-PROGRESS/zigzag.py
@@ -3,6 +3,7 @@ def convert(s: str, numRows: int) -> str:
     jumps = 2 * (numRows - 1) if numRows != 1 else numRows
     size = len(s)
     copy = s[:]
+    idx = []
     i = 0
     j = 0
     k = 1
@@ -13,9 +14,12 @@ def convert(s: str, numRows: int) -> str:
             if (i == 0 and j == 0):
                 j += jumps
                 continue
+            while (jumps == 1 and j < size and j in idx):
+                j += 1
             slist = list(s)
             slist[k] = copy[j]
             s = "".join(slist)
+            idx.append(j)
             k += 1
             j += jumps
         jumps //= 2
@@ -26,3 +30,4 @@ def convert(s: str, numRows: int) -> str:
 
 if __name__ == '__main__':
     print(convert("PAYPALISHIRING", 3))
+    print(convert("PAYPALISHIRING", 4))
