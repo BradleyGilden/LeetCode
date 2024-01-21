d1f4aa93abfe165a34480612cea3f660fb8bcb07
BradleyGilden <bradleygilden@gmail.com>
2024-01-21 10:04:15 +0200

    int to roman

--- int_to_roman/int_to_roman.py
+++ int_to_roman/int_to_roman.py
@@ -6,26 +6,37 @@ Date: 20-01-2024
 """
 
 
-def intToRoman(self, num: int) -> str:
+def intToRoman(num: int) -> str:
     """converts integer to roman numeral"""
+    res = []
     inum = str(num)
-    multiplier = 10 ** (len(inum) - 1)
+    multiplier = len(inum) - 1
     romanDict = {
-        1000: 'M',
-        900: 'CM',
-        500: 'D',
-        400: 'CD',
-        100: 'C',
-        90: 'XC',
-        50: 'L',
-        40: 'XL',
-        10: 'X',
-        9: 'IX',
-        5: 'V',
-        4: 'IV',
-        1: 'I'
+        '1000': 'M',
+        '900': 'CM',
+        '500': 'D',
+        '400': 'CD',
+        '100': 'C',
+        '90': 'XC',
+        '50': 'L',
+        '40': 'XL',
+        '10': 'X',
+        '9': 'IX',
+        '5': 'V',
+        '4': 'IV',
+        '1': 'I'
     }
-    
+    for n in inum:
+        if n in romanDict:
+            res.append(romanDict[n + ('0' * multiplier)])
+        elif n > '5':
+            res.append(romanDict['5' + ('0' * multiplier)])
+            [res.append(romanDict['1' + ('0' * multiplier)]) for _ in range(int(n) - 5)]
+        else:
+            [res.append(romanDict['1' + ('0' * multiplier)]) for _ in range(int(n))]
+        multiplier -= 1
+    return ''.join(res)
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    print(intToRoman(423))
